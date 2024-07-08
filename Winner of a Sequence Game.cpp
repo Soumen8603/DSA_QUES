798e88ac9ac5545084e03252f685046850ac6e7b
Soumen Pal <153191407+Soumen8603@users.noreply.github.com>
2024-07-08 10:24:17 +0530

    Create Winner of a Sequence Game.cpp

---
+++ Winner of a Sequence Game.cpp
@@ -0,0 +1,12 @@
+ int findTheWinner(int n, int k) {
+        vector<int>v(n,0);
+        for(int i=0;i<n;i++){
+            v[i]=i+1;
+        }
+        int index=0;
+        while(v.size()!=1){
+          index=(index+(k-1))%v.size();
+          v.erase(v.begin()+index);
+        }
+        return v[0];
+    }
