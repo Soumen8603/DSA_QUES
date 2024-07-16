14ea87fcdf67bb69b46eb898d47f69ab076e8c7b
Soumen Pal <153191407+Soumen8603@users.noreply.github.com>
2024-07-16 21:33:59 +0530

    Update Step by Step Directiona From a  Binary  Tree Node to Another.cpp

--- Step by Step Directiona From a  Binary  Tree Node to Another.cpp
+++ Step by Step Directiona From a  Binary  Tree Node to Another.cpp
@@ -42,3 +42,42 @@ TreeNode* lca(TreeNode* root,int p,int q){
         for(auto& ch : lcatostart)ch='U';
         return lcatostart+lcatoend;
     }
+
+//Another Approach
+
+void dfs(TreeNode* node , string& pathstring , int p , int q){
+          if(node == nullptr)return;
+          if(node->val == p)lcaToStart=pathstring;
+          if(node->val == q)lcaToEnd=pathstring;
+
+          pathstring.push_back('L');
+          dfs(node->left,pathstring,p,q);
+          pathstring.pop_back();
+
+          pathstring.push_back('R');
+          dfs(node->right,pathstring,p,q);
+          pathstring.pop_back();
+
+    }
+    string getDirections(TreeNode* root, int startValue, int destValue) {
+        string pathstring="";
+        dfs(root,pathstring,startValue,destValue);
+
+        int common=0,i=0,j=0; 
+        while(i < lcaToStart.length() && j<lcaToEnd.length()){
+            if(lcaToStart[i]==lcaToEnd[j]){
+                common++;
+                i++;
+                j++;
+            }
+            else{
+                break;
+            }
+        } 
+        lcaToStart = lcaToStart.substr(common,lcaToStart.length()-common); 
+        lcaToEnd = lcaToEnd.substr(common,lcaToEnd.length()-common); 
+
+        for(auto& ch : lcaToStart)ch='U';
+        return lcaToStart+lcaToEnd;
+
+    }
