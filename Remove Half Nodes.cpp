ee9abee97ba551824a62201675c436a0a054f95c
Soumen Pal <153191407+Soumen8603@users.noreply.github.com>
2024-07-20 08:42:15 +0530

    Create Remove Half Nodes.cpp

---
+++ Remove Half Nodes.cpp
@@ -0,0 +1,14 @@
+Node *RemoveHalfNodes(Node *root) {
+        // code here
+        if(root == NULL)return NULL;
+        
+        root->left = RemoveHalfNodes(root->left);
+        root->right = RemoveHalfNodes(root->right);
+        
+        if(root->left == NULL && root->right != NULL)return root->right;
+        if(root->left != NULL && root->right == NULL)return root->left;
+        
+        return root;
+        
+        
+    }
