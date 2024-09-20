075aca0c108f2d12043f8085dc72c8598bc8d8a4
Soumen Pal <153191407+Soumen8603@users.noreply.github.com>
2024-09-20 19:17:39 +0530

    Create Shortest Palindrome.cpp

---
+++ Shortest Palindrome.cpp
@@ -0,0 +1,30 @@
+void func(string a,vector<int>&lps){
+    int i = 0;
+    int j = 1;
+    while(j < a.length()){
+        if(a[i]==a[j]){
+            lps[j] = i+1;
+            i++;
+            j++;
+        }
+        else{
+            if(i == 0){
+                j++;
+            }
+            else{
+                i = lps[i-1];
+            }
+        }
+    }
+}
+    string shortestPalindrome(string s) {
+        string t = s;
+        reverse(t.begin(),t.end());
+        string a = s + "#" + t;
+        vector<int>lps(a.length(),0);
+        func(a,lps);
+        int i = lps[a.length()-1];
+        string temp = s.substr(i);
+        reverse(temp.begin(),temp.end());
+        return temp+s;
+    }
