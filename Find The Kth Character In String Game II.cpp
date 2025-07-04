721e2981b3474a36ef82c1ade356448de2a4430d
Soumen Pal <153191407+Soumen8603@users.noreply.github.com>
2025-07-04 19:41:18 +0530

    Create Find The Kth Character In String Game II.cpp

---
+++ Find The Kth Character In String Game II.cpp
@@ -0,0 +1,10 @@
+char kthCharacter(long long k, vector<int>& operations) {
+        int count = 0;
+        ll val = k;
+        while(val > 1){
+            int jump = ceil(log2(val));
+            val -= pow(2,jump-1);
+            count += operations[jump-1];
+        }
+        return char('a'+(count % 26));
+    }
