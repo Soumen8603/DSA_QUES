6b73a300e98308a70d5c9f298bc7a77968cd549e
Soumen Pal <153191407+Soumen8603@users.noreply.github.com>
2025-08-08 22:47:54 +0530

    Create KMP_Algo.cpp

---
+++ KMP_Algo.cpp
@@ -0,0 +1,47 @@
+ vector<int> compute_lps(string s) {
+        int m = s.size();
+        vector<int> lps(m, 0);
+        int i = 1;
+        int len = 0;
+        while (i < m) {
+            if (s[i] == s[len]) {
+                len++;
+
+                lps[i] = len;
+                i++;
+            }
+            else{
+                if(len != 0){
+                    len = lps[len-1];
+                }
+                else{
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+        return lps;
+    }
+    int strStr(string haystack, string needle) {
+        int n = haystack.size();
+        int m = needle.size();
+        vector<int>lps =  compute_lps(needle);
+        int i = 0;
+        int j = 0;
+        while(i < n){
+            if(haystack[i] == needle[j]){
+                i++;
+                j++;
+                if(j == m)return i - j;
+            }
+            else if(i < n && haystack[i] != needle[j]){
+                if(j != 0){
+                    j = lps[j - 1];
+                }
+                else{
+                    i++;
+                }
+            }
+        }
+        return -1;
+    }
