04b97928c05dfe81916947206ff191cd69f0a00f
Soumen Pal <153191407+Soumen8603@users.noreply.github.com>
2025-08-13 07:58:48 +0530

    Create Power of Three.cpp

---
+++ Power of Three.cpp
@@ -0,0 +1,9 @@
+bool isPowerOfThree(int n) {
+      
+        int num = 0;
+        for(int i = 0; pow(3,i) <= n; i++){
+            num = pow(3,i);
+            if(num == n) return true;
+        }
+        return false;
+    }
