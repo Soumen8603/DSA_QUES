95f60f7c2c3792cba5f8f592b0d69741f8d10509
Soumen Pal <153191407+Soumen8603@users.noreply.github.com>
2025-09-12 14:47:00 +0530

    Create Ransom Note.cpp

---
+++ Ransom Note.cpp
@@ -0,0 +1,15 @@
+   bool canConstruct(string ransomNote, string magazine) {
+        unordered_map<char,int>mp;
+        for(int i = 0;i < magazine.size();i++){
+            mp[magazine[i]]++;
+        }
+        for(int i = 0;i < ransomNote.size();i++){
+            if(mp[ransomNote[i]] >= 1){
+                mp[ ransomNote[i]]--;
+            }
+            else{
+                return false;
+            }
+        }
+        return true;
+    }
