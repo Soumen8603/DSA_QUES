055f9b736bc8b992311a138e0f8f05d0c0f75620
Soumen Pal <153191407+Soumen8603@users.noreply.github.com>
2025-10-07 21:37:57 +0530

    Create Avoid Flood in The City.cpp

---
+++ Avoid Flood in The City.cpp
@@ -0,0 +1,25 @@
+ vector<int> avoidFlood(vector<int>& rains) {
+         int n = rains.size();
+        vector<int> ans(n, 1);
+        unordered_map<int, int> full;
+        set<int> dryDays;
+
+        for (int i = 0; i < n; i++) {
+            if (rains[i] == 0) {
+                dryDays.insert(i);
+            } else {
+                int lake = rains[i];
+                if (full.count(lake)) {
+                    auto it = dryDays.lower_bound(full[lake]);
+                    if (it == dryDays.end()) {
+                        return {};
+                    }
+                    ans[*it] = lake;
+                    dryDays.erase(it);
+                }
+                full[lake] = i;
+                ans[i] = -1;
+            }
+        }
+        return ans;
+    }
