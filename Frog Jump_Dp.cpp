16c86bf4740b93f2e65c9f860bffacb4a611ab49
Soumen Pal <153191407+Soumen8603@users.noreply.github.com>
2025-11-18 16:45:50 +0530

    Update Frog Jump_Dp.cpp

--- Frog Jump_Dp.cpp
+++ Frog Jump_Dp.cpp
@@ -1,3 +1,4 @@
+//1. O(n) time complexity and O(n) space complexity
 int frogJump(int n, vector<int> &heights)
 {
    if(n == 1)return 0;
@@ -18,4 +19,29 @@ int frogJump(int n, vector<int> &heights)
         dp[i] = min(dp[i-1] + abs(heights[i-1]-heights[i-2]),dp[i-2] + abs(heights[i-1] - heights[i-3]));
     }
     return dp[n];
+}
+//2. O(n) time complexity and O(1) space complexity
+int frogJump(int n, vector<int> &heights)
+{
+   if(n == 1)return 0;
+    // Write your code here.
+    int firstprev = 0;
+    int secondprev = 0;
+    firstprev = abs(heights[1] - heights[0]);
+
+    if(n == 2){
+       return firstprev;
+    }
+    int temp = firstprev;
+    secondprev = temp;
+    firstprev = min(firstprev + abs(heights[2] - heights[1]),abs(heights[2]-heights[0]));
+    if(n == 3){
+        firstprev;
+    }
+    for(int i = 4;i <= n;i++){
+       temp = firstprev;
+       firstprev = min(temp + abs(heights[i-1] - heights[i-2]),secondprev + abs(heights[i-1] - heights[i-3]));
+       secondprev = temp;
+    }
+    return firstprev;
 }
