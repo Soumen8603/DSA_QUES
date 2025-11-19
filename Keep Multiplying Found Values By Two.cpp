3db7fcaa858328d16b3ab26b865d668c56994ec6
Soumen Pal <153191407+Soumen8603@users.noreply.github.com>
2025-11-19 10:48:23 +0530

    Create Keep Multiplying Found Values By Two.cpp

---
+++ Keep Multiplying Found Values By Two.cpp
@@ -0,0 +1,9 @@
+ int findFinalValue(vector<int>& nums, int original) {
+        sort(nums.begin(),nums.end());
+        for(int i = 0;i < nums.size();i++){
+            if(nums[i] == original){
+                original *= 2;
+            }
+        }
+        return original;
+    }
