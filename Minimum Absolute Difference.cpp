9ef434a27dd69f307a5fc7b13f9a42ce41d94f14
Soumen Pal <153191407+Soumen8603@users.noreply.github.com>
2026-01-26 18:08:49 +0530

    Create Minimum Absolute Difference.cpp

---
+++ Minimum Absolute Difference.cpp
@@ -0,0 +1,15 @@
+vector<vector<int>> minimumAbsDifference(vector<int>& arr) {
+        int mini = INT_MAX;
+        int n = arr.size();
+        sort(arr.begin(),arr.end());
+        for(int i = 0;i < n-1;i++){
+            mini = min(mini,arr[i+1]-arr[i]);
+        }
+        vector<vector<int>>ans;
+        for(int i = 0;i < n-1;i++){
+            if(arr[i+1]-arr[i] == mini){
+                ans.push_back({arr[i],arr[i+1]});
+            }
+        }
+        return ans;
+    }
